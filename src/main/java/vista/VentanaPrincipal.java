43aa65335b3e9b953f6fb4ece33aef13f265aba9
dopa <edu.riveros@duocuc.cl>
2026-09-15 22:53:59 -0300

    1

---
+++ src/main/java/vista/VentanaPrincipal.java
@@ -0,0 +1,11 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JButton registrarPedidoButton;
+    private JButton salirButton;
+    private JButton listarPedidoButton;
+    private JButton asignarRepartidorButton1;
+    private JButton iniciarEntregaButton1;
+}
